8656c28b8d7b56af190fcc85291b07d0a15cb385
ariszak98 <ariszak98@icloud.com>
2024-05-23 17:48:35 +0300

    Added Views: About, Projects, Contact

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,7 @@
+@section('title', 'Welcome | Zakynthinos A.')
+
 <x-layout>
-    
+
 
     <!-- INTRO -->
     <section id="intro" class="s-intro">
@@ -89,27 +91,33 @@
 
                     <ul class="s-about__social social-list">
                         <li>
-                            <a href="#0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(0, 0, 0, 1);transform:;-ms-filter:"><path d="M20,3H4C3.447,3,3,3.448,3,4v16c0,0.552,0.447,1,1,1h8.615v-6.96h-2.338v-2.725h2.338v-2c0-2.325,1.42-3.592,3.5-3.592 c0.699-0.002,1.399,0.034,2.095,0.107v2.42h-1.435c-1.128,0-1.348,0.538-1.348,1.325v1.735h2.697l-0.35,2.725h-2.348V21H20 c0.553,0,1-0.448,1-1V4C21,3.448,20.553,3,20,3z"></path></svg>
-                                <span class="u-screen-reader-text">Facebook</span>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                                <span class="u-screen-reader-text">Github</span>
                             </a>
                         </li>
+                        
+                        
+                        
                         <li>
-                            <a href="#0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(0, 0, 0, 1);transform:;-ms-filter:"><path d="M19.633,7.997c0.013,0.175,0.013,0.349,0.013,0.523c0,5.325-4.053,11.461-11.46,11.461c-2.282,0-4.402-0.661-6.186-1.809 c0.324,0.037,0.636,0.05,0.973,0.05c1.883,0,3.616-0.636,5.001-1.721c-1.771-0.037-3.255-1.197-3.767-2.793 c0.249,0.037,0.499,0.062,0.761,0.062c0.361,0,0.724-0.05,1.061-0.137c-1.847-0.374-3.23-1.995-3.23-3.953v-0.05 c0.537,0.299,1.16,0.486,1.82,0.511C3.534,9.419,2.823,8.184,2.823,6.787c0-0.748,0.199-1.434,0.548-2.032 c1.983,2.443,4.964,4.04,8.306,4.215c-0.062-0.3-0.1-0.611-0.1-0.923c0-2.22,1.796-4.028,4.028-4.028 c1.16,0,2.207,0.486,2.943,1.272c0.91-0.175,1.782-0.512,2.556-0.973c-0.299,0.935-0.936,1.721-1.771,2.22 c0.811-0.088,1.597-0.312,2.319-0.624C21.104,6.712,20.419,7.423,19.633,7.997z"></path></svg>
-                                <span class="u-screen-reader-text">Twitter</span>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                                  
+                                  <span class="u-screen-reader-text">Linkedin</span>                           
                             </a>
                         </li>
+
+                        <li>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" id="leetcode"><path d="M22,14.355c0-0.742-0.564-1.346-1.26-1.346H10.676c-0.696,0-1.26,0.604-1.26,1.346s0.563,1.346,1.26,1.346H20.74C21.436,15.702,22,15.098,22,14.355z"></path><path d="M3.482,18.187l4.313,4.361C8.768,23.527,10.113,24,11.598,24c1.485,0,2.83-0.512,3.805-1.494l2.588-2.637c0.51-0.514,0.492-1.365-0.039-1.9c-0.531-0.535-1.375-0.553-1.884-0.039l-2.676,2.607c-0.462,0.467-1.102,0.662-1.809,0.662s-1.346-0.195-1.81-0.662l-4.298-4.363c-0.463-0.467-0.696-1.15-0.696-1.863c0-0.713,0.233-1.357,0.696-1.824l4.285-4.38c0.463-0.467,1.116-0.645,1.822-0.645s1.346,0.195,1.809,0.662l2.676,2.606c0.51,0.515,1.354,0.497,1.885-0.038c0.531-0.536,0.549-1.387,0.039-1.901l-2.588-2.636c-0.649-0.646-1.471-1.116-2.392-1.33l-0.034-0.007l2.447-2.503c0.512-0.514,0.494-1.366-0.037-1.901c-0.531-0.535-1.376-0.552-1.887-0.038L3.482,10.476C2.509,11.458,2,12.813,2,14.311C2,15.809,2.509,17.207,3.482,18.187z"></path></svg>
+                                <span class="u-screen-reader-text">Leetcode</span>  
+                            </a>
+                        </li>
+
                         <li>
                             <a href="#0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(0, 0, 0, 1);transform:;-ms-filter:"><path d="M11.999,7.377c-2.554,0-4.623,2.07-4.623,4.623c0,2.554,2.069,4.624,4.623,4.624c2.552,0,4.623-2.07,4.623-4.624 C16.622,9.447,14.551,7.377,11.999,7.377L11.999,7.377z M11.999,15.004c-1.659,0-3.004-1.345-3.004-3.003 c0-1.659,1.345-3.003,3.004-3.003s3.002,1.344,3.002,3.003C15.001,13.659,13.658,15.004,11.999,15.004L11.999,15.004z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533,6.111c-0.469-1.209-1.424-2.165-2.633-2.632c-0.699-0.263-1.438-0.404-2.186-0.42 c-0.963-0.042-1.268-0.054-3.71-0.054s-2.755,0-3.71,0.054C7.548,3.074,6.809,3.215,6.11,3.479C4.9,3.946,3.945,4.902,3.477,6.111 c-0.263,0.7-0.404,1.438-0.419,2.186c-0.043,0.962-0.056,1.267-0.056,3.71c0,2.442,0,2.753,0.056,3.71 c0.015,0.748,0.156,1.486,0.419,2.187c0.469,1.208,1.424,2.164,2.634,2.632c0.696,0.272,1.435,0.426,2.185,0.45 c0.963,0.042,1.268,0.055,3.71,0.055s2.755,0,3.71-0.055c0.747-0.015,1.486-0.157,2.186-0.419c1.209-0.469,2.164-1.424,2.633-2.633 c0.263-0.7,0.404-1.438,0.419-2.186c0.043-0.962,0.056-1.267,0.056-3.71s0-2.753-0.056-3.71C20.941,7.57,20.801,6.819,20.533,6.111z M19.315,15.643c-0.007,0.576-0.111,1.147-0.311,1.688c-0.305,0.787-0.926,1.409-1.712,1.711c-0.535,0.199-1.099,0.303-1.67,0.311 c-0.95,0.044-1.218,0.055-3.654,0.055c-2.438,0-2.687,0-3.655-0.055c-0.569-0.007-1.135-0.112-1.669-0.311 c-0.789-0.301-1.414-0.923-1.719-1.711c-0.196-0.534-0.302-1.099-0.311-1.669c-0.043-0.95-0.053-1.218-0.053-3.654 c0-2.437,0-2.686,0.053-3.655c0.007-0.576,0.111-1.146,0.311-1.687c0.305-0.789,0.93-1.41,1.719-1.712 c0.534-0.198,1.1-0.303,1.669-0.311c0.951-0.043,1.218-0.055,3.655-0.055c2.437,0,2.687,0,3.654,0.055 c0.571,0.007,1.135,0.112,1.67,0.311c0.786,0.303,1.407,0.925,1.712,1.712c0.196,0.534,0.302,1.099,0.311,1.669 c0.043,0.951,0.054,1.218,0.054,3.655c0,2.436,0,2.698-0.043,3.654H19.315z"></path></svg>
                                 <span class="u-screen-reader-text">Instagram</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20.66 6.98a9.932 9.932 0 0 0-3.641-3.64C15.486 2.447 13.813 2 12 2s-3.486.447-5.02 1.34c-1.533.893-2.747 2.107-3.64 3.64S2 10.187 2 12s.446 3.487 1.34 5.02a9.924 9.924 0 0 0 3.641 3.64C8.514 21.553 10.187 22 12 22s3.486-.447 5.02-1.34a9.932 9.932 0 0 0 3.641-3.64C21.554 15.487 22 13.813 22 12s-.446-3.487-1.34-5.02zM12 3.66c2 0 3.772.64 5.32 1.919-.92 1.174-2.286 2.14-4.1 2.9-1.002-1.813-2.088-3.327-3.261-4.54A7.715 7.715 0 0 1 12 3.66zM5.51 6.8a8.116 8.116 0 0 1 2.711-2.22c1.212 1.201 2.325 2.7 3.34 4.5-2 .6-4.114.9-6.341.9-.573 0-1.006-.013-1.3-.04A8.549 8.549 0 0 1 5.51 6.8zM3.66 12c0-.054.003-.12.01-.2.007-.08.01-.146.01-.2.254.014.641.02 1.161.02 2.666 0 5.146-.367 7.439-1.1.187.373.381.793.58 1.26-1.32.293-2.674 1.006-4.061 2.14S6.4 16.247 5.76 17.5c-1.4-1.587-2.1-3.42-2.1-5.5zM12 20.34c-1.894 0-3.594-.587-5.101-1.759.601-1.187 1.524-2.322 2.771-3.401 1.246-1.08 2.483-1.753 3.71-2.02a29.441 29.441 0 0 1 1.56 6.62 8.166 8.166 0 0 1-2.94.56zm7.08-3.96a8.351 8.351 0 0 1-2.58 2.621c-.24-2.08-.7-4.107-1.379-6.081.932-.066 1.765-.1 2.5-.1.799 0 1.686.034 2.659.1a8.098 8.098 0 0 1-1.2 3.46zm-1.24-5c-1.16 0-2.233.047-3.22.14a27.053 27.053 0 0 0-.68-1.62c2.066-.906 3.532-2.006 4.399-3.3 1.2 1.414 1.854 3.027 1.96 4.84-.812-.04-1.632-.06-2.459-.06z"></path></svg>
-                                <span class="u-screen-reader-text">Dribbble</span>
                             </a>
                         </li>
                     </ul> <!-- end s-footer__social -->
@@ -120,6 +128,94 @@
         
     </section>
     <!-- END ABOUT -->
+
+    <section class="s-testimonials">
+
+        <div class="s-testimonials__header row row-x-center text-center">
+            <div class="column xl-8 lg-12">
+
+                <p class="text-pretitle">
+                Testimonials
+                </p>
+                <h3>
+                Reviews From Real Clients
+                </h3>                        
+
+            </div>
+        </div> <!--end s-testimonials__header -->
+
+        <div class="row s-testimonials__content">
+            <div class="column xl-12 testimonials">
+
+                <div class="swiper-container testimonials__slider page-slider">
+
+                    <div class="swiper-wrapper">
+                        <div class="testimonials__slide swiper-slide">
+                            <p>
+                            Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Voluptas tempore rem. 
+                            Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum.
+                            Laudantium quia consequatur molestias.
+                            </p>
+                            <div class="testimonials__author">
+                                <img src="images/avatars/user-01.jpg" alt="Author image" class="testimonials__avatar">
+                                <cite class="testimonials__cite">
+                                    <strong>John Rockefeller</strong>
+                                    <span>Standard Oil Co.</span>
+                                </cite>
+                            </div>
+                        </div>
+                        <div class="testimonials__slide swiper-slide">
+                            <p>
+                            Voluptas tempore rem. Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. 
+                            Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum.
+                            Laudantium quia consequatur molestias.
+                            </p>
+                            <div class="testimonials__author">
+                                <img src="images/avatars/user-04.jpg" alt="Author image" class="testimonials__avatar">
+                                <cite class="testimonials__cite">
+                                    <strong>Andrew Carnegie</strong>
+                                    <span>Carnegie Steel Co.</span>
+                                </cite>
+                            </div>
+                        </div>
+                        <div class="testimonials__slide swiper-slide">
+                            <p>
+                            Nisi dolores quaerat fuga rem nihil nostrum. Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. 
+                            Voluptas tempore rem. Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. 
+                            Laudantium quia consequatur molestias.
+                            </p>
+                            <div class="testimonials__author">
+                                <img src="images/avatars/user-06.jpg" alt="Author image" class="testimonials__avatar">
+                                <cite class="testimonials__cite">
+                                    <strong>Henry Ford</strong>
+                                    <span>Ford Motor Co.</span>
+                                </cite>
+                            </div>
+                        </div>
+                        <div class="testimonials__slide swiper-slide">
+                            <p>
+                            Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Voluptas tempore rem. 
+                            Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum.
+                            Laudantium quia consequatur molestias.
+                            </p>
+                            <div class="testimonials__author">
+                                <img src="images/avatars/user-02.jpg" alt="Author image" class="testimonials__avatar">
+                                <cite class="testimonials__cite">
+                                    <strong>John Morgan</strong>
+                                    <span>JP Morgan & Co.</span>
+                                </cite>
+                            </div>
+                        </div> 
+                    </div> <!-- end swiper-wrapper --> 
+
+                    <div class="swiper-pagination"></div>
+
+                </div> <!--end testimonials__slider --> 
+
+            </div> <!-- testimonials -->
+        </div> <!--end s-testimonials__content -->
+
+   </section> <!-- end s-testimonials -->
 
 
 </x-layout>
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,121 @@
+@section('title', 'My Projects | Zakynthinos A.')
+
+<x-layout>
+
+
+    <!-- PROJECTS -->
+        <section id="content" class="s-content">
+
+            <section class="s-pageheader pageheader">
+                 <div class="row">
+                     <div class="column xl-12">
+                         <h1 class="page-title">
+                             <span class="page-title__small-type text-pretitle">Projects</span>
+                             Active & non Projects
+                         </h1>
+                         
+                     </div>
+                 </div>
+            </section> <!-- pageheader -->  
+
+            <section class="s-pagecontent pagecontent">
+
+                 <div class="row pageintro">
+                     <div class="column xl-6 lg-12">
+                         <h2 class="text-display-title">High-impact services to help your business</h2>
+                     </div>
+                     <div class="column xl-6 lg-12 u-flexitem-x-right">
+                         <p class="lead">
+                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias eos quas blanditiis, quos sint nostrum fugit aperiam 
+                         inventore optio itaque molestias corporis, ipsa tenetur eligendi nihil iste porro, natus culpa consequuntur? Maxime, 
+                         corporis tempore. Sed tenetur veritatis velit recusandae eum, molestiae voluptate ducimus laudantium esse illo 
+                         doloribus atque eligendi deleniti iusto. 
+                         </p>
+                     </div>                       
+                 </div>  <!-- pageintro -->       
+
+                 <div class="row">
+                     <div class="column xl-12 grid-block">             
+                         
+                         <div class="grid-full grid-list-items list-items show-ctr">
+                             
+                             <div style="margin-top: 2em" class="grid-list-items__item list-items__item">
+                                 <div class="list-items__item-header">
+                                     <h3 style="font-weight: semibold; font-size: 1.7em;" class="list-items__item-title">
+                                        <a target="__blank" href="#" class="hover:text-blue-500">Digital Marketing</a>
+                                    </h3>
+                                 </div>
+                                 <div class="list-items__item-text">
+                                     <p>
+                                     Rerum quam quos. Aut asperiores 
+                                     sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut 
+                                     magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.
+                                     </p>
+                                     <br>
+                                     <ul class="list-services pt-4">
+                                         <li>Cumque Ducimus</li>
+                                         <li>Maxime Vel</li>
+                                         <li>Eligendi Et</li>
+                                     </ul>
+                                 </div>
+                             </div> <!-- end list-items__item -->
+
+                             
+                             <div style="margin-top: 2em" class="grid-list-items__item list-items__item">
+                                 <div class="list-items__item-header">
+                                     <h3 style="font-weight: semibold; font-size: 1.7em;" class="list-items__item-title">
+                                        <a target="__blank" href="#" class="hover:text-blue-500">Social Media Marketing</a>
+                                    </h3>
+                                 </div>
+                                 <div class="list-items__item-text">
+                                     <p>
+                                     Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores 
+                                     sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut 
+                                     magnam.
+                                     </p>
+                                     <br>
+                                     <ul class="list-services">
+                                         <li>Lorem Ipsum</li>
+                                         <li>voluptatibus Earum</li>
+                                         <li>Cumque Ducimus</li>
+                                     </ul>
+                                 </div>
+                             </div> <!-- end list-items__item -->
+
+                             <div style="margin-top: 2em" class="grid-list-items__item list-items__item">
+                                 <div class="list-items__item-header">
+                                     <h3 style="font-weight: semibold; font-size: 1.7em;" class="list-items__item-title">
+                                        <a target="__blank" href="#" class="hover:text-blue-500">Content Marketing</a>
+                                    </h3>
+                                 </div>
+                                 <div class="list-items__item-text">
+                                     <p>
+                                     Rerum quam quos. Quibusdam quis autem voluptatibus earum vel ex error ea magni. Aut asperiores 
+                                     sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut 
+                                     magnam.
+                                     </p>
+                                     <br>
+                                     <ul class="list-services">
+                                         <li>Eaque velit</li>
+                                         <li>Asperiores</li>
+                                         <li>Cumque Ducimus</li>
+                                     </ul>
+                                 </div>
+                             </div> <!-- end list-items__item -->
+                             
+ 
+                         </div> <!-- grid-list-items -->
+                        
+
+                     </div> <!-- end grid-block-->
+                 </div> <!-- end row -->
+
+            </section> <!-- pagecontent -->
+
+           
+
+     </section>
+     <!-- END PROJECTS -->
+
+
+</x-layout>
