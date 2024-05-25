f38a4a08628599da853587d12fdd7a3b9ec8ad1f
ariszak98 <ariszak98@icloud.com>
2024-05-25 14:38:18 +0300

    Functionality to block new accounts if 'admin' name value in teble.users exists + added hidden input for name=admin in view register so that the first register is always the admin

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,19 +36,13 @@
         @csrf
 
         <div>
-          <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-          <div class="mt-2">
-            <input id="name" name="name" autocomplete="" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-          @error('name')
-              <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-          @enderror
+          <input type="hidden" name="name" value="admin">
         </div>
 
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input id="email" name="email" value="{{ old('email') }}" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
           @error('email')
               <p class="text-sm text-red-500 mt-1">You need to provide a legit email.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SessionController;
-use Illuminate\Support\Facades\Route;
 
 
 // Guest
@@ -10,8 +11,16 @@ Route::middleware('guest')->group( function() {
 
     Route::get('/login', [SessionController::class, 'create' ])->name('login');
     Route::post('/login', [SessionController::class, 'store']);
-    Route::get('/register', [AdminController::class, 'create' ]);
-    Route::post('/register', [AdminController::class, 'store']);
+
+    // Check if "admin" exists
+    if (!User::where('name', 'admin')->exists()){
+        Route::get('/register', [AdminController::class, 'create' ]);
+        Route::post('/register', [AdminController::class, 'store']);
+    } else {
+        Route::get('/register', function() { return redirect('/'); });
+        Route::post('/register', function() { return redirect('/'); });
+    }
+    
 
 });
 
